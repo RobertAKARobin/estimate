04e76b578568cd743c04770cab9598e26f65341c
RobertAKARobin <robertgfthomas@gmail.com>
2016-11-16 16:12:14 -0600

    Added cookie

--- main.js
+++ main.js
@@ -87,6 +87,14 @@
 						HubspotForm.setFieldValue(linkFieldId, vm.saveLink);
 					});
 				}
+			})
+			.then(function hubspotAnalytics(){
+				(function(d,s,i,r) {
+					if (d.getElementById(i)){return;}
+					var n=d.createElement(s),e=d.getElementsByTagName(s)[0];
+					n.id=i;n.src='//js.hs-analytics.net/analytics/'+(Math.ceil(new Date()/r)*r)+'/211554.js';
+					e.parentNode.insertBefore(n, e);
+				})(document,"script","hs-analytics",300000);
 			});
 
 		function reset(){
